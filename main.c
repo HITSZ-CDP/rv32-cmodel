6d32338d160c5d42a51b72753307652af48f833b
Bohan Hu <hubohancser@gmail.com>
2021-06-23 19:38:15 +0800

    Add arg support

--- main.c
+++ main.c
@@ -5,11 +5,13 @@ extern riscv32_CPU_state cpu;
 extern void init_memory(char*);
 extern int cpu_run_once();
 extern void print_reg_state();
-int main(void){
-    memory[0] = 0x00300093;
-    memory[1] = 0x73;
+int main(int argc, char* argv[]){
     cpu.npc = 0;
-    init_memory("addi.bin");
+    if(argc != 2) {
+        printf("Usage: ./emu xxx.bin\n");
+        return -1;
+    }
+    init_memory(argv[1]);
     Log("CPU Started\n");
     int state = 0;
     cpu.pc = 0;
